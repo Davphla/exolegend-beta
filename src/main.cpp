12aa7ecc5626b1d8efc8a1429794174b9c487543
m-brl <mathieu.borel@epitech.eu>
2025-03-07 14:51:58 +0100

    feat(offset): add offset
    Signed-off-by: m-brl <mathieu.borel@epitech.eu>

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include "gladiator.h"
 Gladiator *gladiator;
-void reset();
-void setup()
-{
-    // instanciation de l'objet gladiator
-    gladiator = new Gladiator();
-    // enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
-    gladiator->game->onReset(&reset); // GFA 4.4.1
-}
+
+#define ROBOT_ID 80
+
+#if defined(ROBOT_ID) && ROBOT_ID == 79
+    #define DELTA_X 0.0047
+    #define DELTA_Y -0.0046
+#elif defined(ROBOT_ID) && ROBOT_ID == 80
+    #define DELTA_X  0.0028
+    #define DELTA_Y -0.0069
+#endif 
 
 void reset()
 {
@@ -16,14 +18,26 @@ void reset()
     gladiator->log("Call of reset function"); // GFA 4.5.1
 }
 
+void setup()
+{
+    // instanciation de l'objet gladiator
+    gladiator = new Gladiator();
+    // enregistrement de la fonction de reset qui s'éxecute à chaque fois avant qu'une partie commence
+    gladiator->game->onReset(&reset); // GFA 4.4.1
+    gladiator->game->enableFreeMode(RemoteMode::OFF);
+}
+
 void loop()
 {
+    MazeSquare ms;
+    gladiator->robot->setCalibrationOffset(DELTA_X, DELTA_Y, 0); //TODO CHANGE A CALIBRATION OFFSET
+    gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0.2); 
+    gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0.2);
+
     if (gladiator->game->isStarted())
     { // tester si un match à déjà commencer
         // code de votre stratégie
         gladiator->log("Hello world - Game Started"); // GFA 4.5.1
-        gladiator->control->setWheelSpeed(WheelAxis::LEFT, 0.2); 
-        gladiator->control->setWheelSpeed(WheelAxis::RIGHT, 0.2); 
     }
     else
     {
