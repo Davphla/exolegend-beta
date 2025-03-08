21ba26ece46fe1a65882c1599de492ab5a364b4d
david <david.gozlan@epitech.eu>
2025-03-08 14:02:27 +0100

    feat: added nb_visit to metrics

--- src/Gladiator.cpp
+++ src/Gladiator.cpp
@@ -12,7 +12,8 @@ struct coordinate_t {
 };
 
 struct square_metric_t {
-    uint score;
+    uint score; // closest neighboor
+    uint nb_visit; // nb time player visit this square
 };
 
 using metrics_t = std::map<coordinate_t, square_metric_t>;
